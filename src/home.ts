07aeba2f31fc0f59c15ac6ad9e47bf8d1d5b13a8
one23four56 <jason@baydq.com>
2023-07-03 18:51:49 -0500

    add importing

--- src/home.ts
+++ src/home.ts
@@ -1,4 +1,4 @@
-import { id, Project, set, get } from ".";
+import { id, Project, set, get, isProject, alert } from ".";
 
 id("create-button").addEventListener("click", () => {
 
@@ -50,3 +50,47 @@ for (const project of projects) {
     id("main").appendChild(document.createElement("hr"));
 
 }
+
+id("import-button").addEventListener("click", () => {
+
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".bingo"
+
+    input.addEventListener("change", async () => {
+        if (!input.files || !input.files[0])
+            return;
+
+        const file = input.files[0], text = await file.text();
+
+        try {
+            const object = JSON.parse(text);
+            let endMessage: string | null = null;
+            
+            if (!isProject(object))
+                throw new Error("Extracted object does not properly implement Project");
+
+            if (get<string[]>("index").includes(object.id)) {
+                object.id = crypto.randomUUID();
+                endMessage = `A project with the same ID already existed, so the ID of the uploaded project has been updated.`
+            }
+
+            set("index", [...get<string[]>("index"), object.id]);
+            set(object.id, object);
+
+            await alert(
+                `The project '${object.name} Bingo' has been imported successfully.` +
+                (endMessage ? `\n(Note: ${endMessage})` : ``)
+            )
+
+            window.open(`project.html?project=${object.id}`, "_self")
+            
+        } catch (err) {
+            alert(`The file '${file.name}' is invalid.\n(${err})\nPlease make sure you upload a valid .bingo file`);
+        }
+    })
+
+    input.click();
+    setTimeout(() => input.remove(), 0);
+
+})
--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,38 @@ export interface Project {
     formatVersion: 1; // for backwards compatibility if format is ever updated
 }
 
-export function alert(text: string) {
+/**
+ * Checks whether or not an object is a {@link Project}
+ * @param object object to check
+ * @returns whether or not the object implements {@link Project}
+ */
+export function isProject(object: unknown): object is Project {
+    return (
+        object &&
+        typeof object === "object" &&
+        typeof object["id"] === "string" &&
+        typeof object["name"] === "string" &&
+        Array.isArray(object["items"]) &&
+        object["items"].every(i => typeof i === "string") &&
+        typeof object["time"] === "number" &&
+        typeof object["freeSpace"] === "boolean" &&
+        object["formatVersion"] === 1
+    ) as boolean;
+}
+
+/**
+ * Opens a popup that displays some text
+ * @param text text to display
+ */
+export function alert(text: string): Promise<void> {
     id("alert-text").innerText = text;
     id<HTMLDialogElement>("alert").showModal();
-    id("alert-close").addEventListener("click",
-        () => id<HTMLDialogElement>("alert").close(),
-    { once: true });
+
+    return new Promise((resolve) => {
+        id("alert-close").addEventListener("click", () => {
+            id<HTMLDialogElement>("alert").close();
+            resolve();
+        }, { once: true });
+    });
+
 }
