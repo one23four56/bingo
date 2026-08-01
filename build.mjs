610b2299d1a6989d955001bbc01b5323f6c8cccf
jason mayer <72141247+jason-mayer@users.noreply.github.com>
2026-07-31 23:05:08 -0500

    update build script (#9)

--- build.mjs
+++ build.mjs
@@ -16,6 +16,16 @@ const pack = process.argv[2] === "pack";
 console.log(`[build]: building in ${pack ? "pack" : "dev"} mode`);
 console.time("[build]: build completed in");
 
+const existing = await Promise.all(
+    entryPoints
+        .flatMap((e) => [`static/${e}.js`, `static/${e}.js.map`])
+        .map(file => fs.unlink(file).then(_ => true, _ => false))
+).then(r => r.filter(v => v).length);
+
+if (existing != 0) {
+    console.log(`[build]: removed ${existing} existing file${existing == 1 ? "" : "s"}`);
+}
+
 const context = await esbuild.context({
     entryPoints: entryPoints.map(e => "src/" + e + ".ts"),
     bundle: true,
@@ -27,21 +37,17 @@ const context = await esbuild.context({
     },
     minify: pack,
     sourcemap: !pack,
-
 });
 
-console.timeEnd("[build]: build completed in");
-
 if (pack) {
-    for (const file of entryPoints) {
-        try {
-            await fs.unlink(`static/${file}.js.map`);
-        } catch { }
-    }
+    await context.rebuild();
+    console.timeEnd("[build]: build completed in");
+
     console.log(`[build]: \`static\` folder ready for production`)
     process.exit(0);
 } else {
     await context.watch();
+    console.timeEnd("[build]: build completed in");
     console.log("[build]: watch mode is enabled. any file changes will trigger automatic recompilation")
     const { port } = await context.serve({
         servedir: "static",
